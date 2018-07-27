c07ccada1d4dff8a6927eaff5d8687309d99b091
okossa <okossa.douniama@gmail.com>
2018-07-27 00:35:09 -0400

    small experiment with xamarin

---
+++ xamarin/controls_experiments/controls_experiments/controls_experiments/PictureAttachmentView.xaml.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+using Xamarin.Forms;
+using Xamarin.Forms.Xaml;
+
+namespace controls_experiments
+{
+	[XamlCompilation(XamlCompilationOptions.Compile)]
+	public partial class PictureAttachmentView : ContentView
+	{
+
+        public static readonly BindableProperty TextProperty = BindableProperty.Create(
+               nameof(Text),
+               typeof(string),
+               typeof(PictureAttachmentView),
+               string.Empty,
+               propertyChanging: (bindable, oldValue, newValue) =>
+               {
+                   var control = bindable as PictureAttachmentView;
+                   var changingFrom = oldValue as string;
+                   var changingTo = newValue as string;
+                   control.fileNameLabel.SetValue(Label.TextProperty, newValue);
+               });
+
+        public string Text { get; set; }
+ 
+        public PictureAttachmentView ()
+		{
+			InitializeComponent ();
+        }
+
+        //delete the attachment
+        public void onDeleteAttachment()
+        {
+            IsVisible = false;
+            //header.BackgroundColor = Color.Red;
+        }
+	}
+}
